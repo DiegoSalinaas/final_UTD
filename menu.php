e28efdc43d0f3721673ae5c4025bfc1688cbfd3a
DiegoSalinaas <diegocantero1382@gmail.com>
2025-08-01 16:47:24 -0300

    modified:   menu.php

--- menu.php
+++ menu.php
@@ -60,6 +60,8 @@
       crossorigin="anonymous"
     />
     <link rel="stylesheet" href="css/sweetalert2/sweetalert2.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
   </head>
   <!--end::Head-->
   <!--begin::Body-->
