52ae50a02ef329b94dbf862a3714bd3c94c3f877
DiegoSalinaas <diegocantero1382@gmail.com>
2025-08-04 14:08:26 -0300

    modified:   menu.php

--- menu.php
+++ menu.php
@@ -79,6 +79,7 @@
             <li class="nav-item">
               <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
                 <i class="bi bi-list"></i>
+                
               </a>
             </li>
             <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Inicio</a></li>
@@ -272,23 +273,22 @@
       <!--begin::Sidebar-->
       <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
         <!--begin::Sidebar Brand-->
-        <div class="sidebar-brand">
-          <!--begin::Brand Link-->
-          <a href="./index.html" class="brand-link">
-            <!--begin::Brand Image-->
-            <img
-              src="dist/assets/img/AdminLTELogo.png"
-              alt="AdminLTE Logo"
-              class="brand-image opacity-75 shadow"
-            />
-            <!--end::Brand Image-->
-            <!--begin::Brand Text-->
-            <span class="brand-text fw-light">UTCD INFO</span>
-            <!--end::Brand Text-->
-          </a>
-          <!--end::Brand Link-->
-        </div>
-        <!--end::Sidebar Brand-->
+<div class="sidebar-brand text-center">
+  <!--begin::Brand Link-->
+  <a href="./index.html" class="brand-link d-flex flex-column align-items-center py-2">
+    <!--begin::Brand Image-->
+    <img
+      src="images/logo.png"
+      alt="Logo"
+      class="shadow"
+      style="height: 150px; max-width: 100%; object-fit: contain;"
+    />
+    <!--end::Brand Image-->
+  </a>
+  <!--end::Brand Link-->
+</div>
+<!--end::Sidebar Brand-->
+
         <!--begin::Sidebar Wrapper-->
         <div class="sidebar-wrapper">
           <nav class="mt-2">
